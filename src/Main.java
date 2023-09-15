27e066ff09525eccb697c938762296b9df11144b
rusiente <106997966+rusiente@users.noreply.github.com>
2023-09-15 19:01:24 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+        int numeroA = 900;
+        int numeroB = 500;
+        int suma = numeroA + numeroB;
+        int resta = numeroA - numeroB;
+        int multp = numeroA * numeroB;
+        int division = numeroA / numeroB;
+        int modulo = numeroA % numeroB;
+
+        System.out.println("La suma de " + numeroA + " y " + numeroB + " es " + suma);
+        System.out.println("La resta de " + numeroA + " y " + numeroB + " es " + resta);
+        System.out.println("La multiplicación de " + numeroA + " y " + numeroB + " es " + multp);
+        System.out.println("La división de " + numeroA + " y " + numeroB + " es " + division);
+        System.out.println("El módulo de " + numeroA + " y " + numeroB + " es " + modulo);
+
+    }
+}
